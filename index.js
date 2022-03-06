1db5d8a993c8007d0594921d63a7911041471517
hager-shiref <hagershreif5@gmail.com>
2022-03-06 17:12:01 +0200

    quotes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello Java Script !");
+
+var divide = 4.2/2.0;
+console.log(divide);
+var reminder = 4.2%2.0;
+console.log(reminder);
+const myStr = "I am , \"double  quoted\" string inside \"double quotes\"" // double quotes 
+console.log(myStr)
+const myStr2='Iam , "double  quoted" String inside "Single quotes"' //single quotes
+console.log(myStr2)
